242efdd2d7d79bbf4d5eb4d38b62c57c9d5675f6
QuantumTacoNinja <worapat1h@gmail.com>
2025-01-30 06:29:21 +0700

    drop down rent_log, add foreign keys.

--- main.py
+++ main.py
@@ -8,25 +8,40 @@ if not os.path.exists(db_folder):
 con = sqlite3.connect(f"{db_folder}/maydata.db")
 cursor = con.cursor()
 
-cursor.execute("CREATE TABLE IF NOT EXISTS members (id INTEGER PRIMARY KEY, username TEXT NOT NULL, email TEXT NOT NULL)")
-cursor.execute("CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY, title TEXT NOT NULL, author TEXT NOT NULL)")
-cursor.execute("CREATE TABLE IF NOT EXISTS rent_log (id INTEGER PRIMARY KEY, renter TEXT NOT NULL, renter_id INTEGER NOT NULL, title TEXT NOT NULL, book_id INTEGER NOT NULL)")
+# cursor.execute("CREATE TABLE IF NOT EXISTS members (id INTEGER PRIMARY KEY, username TEXT NOT NULL, email TEXT NOT NULL)")
+# cursor.execute("CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY, title TEXT NOT NULL, author TEXT NOT NULL)")
+# cursor.execute("CREATE TABLE IF NOT EXISTS rent_log (id INTEGER PRIMARY KEY, renter TEXT NOT NULL, renter_id INTEGER NOT NULL, title TEXT NOT NULL, book_id INTEGER NOT NULL)")
 
-members_data = [
-    ("Josh", "josh@email.com"),
-    ("Sean", "sean@email.com"),
-    ("Luke", "luke@email.com")
-]
+# members_data = [
+#     ("Josh", "josh@email.com"),
+#     ("Sean", "sean@email.com"),
+#     ("Luke", "luke@email.com")
+# ]
 
-books_data = [
-    ("Fun Novel", "Funny Man"),
-    ("Mathematic", "Smart Man"),
-    ("Brew Tea", "How to Easy"),
-    ("Love chips", "Intel Person")
-]
+# books_data = [
+#     ("Fun Novel", "Funny Man"),
+#     ("Mathematic", "Smart Man"),
+#     ("Brew Tea", "How to Easy"),
+#     ("Love chips", "Intel Person")
+# ]
 
-cursor.executemany("INSERT INTO members (username, email) VALUES (?, ?)", members_data)
-cursor.executemany("INSERT INTO books (title, author) VALUES (?, ?)", books_data)
+# cursor.executemany("INSERT INTO members (username, email) VALUES (?, ?)", members_data)
+# cursor.executemany("INSERT INTO books (title, author) VALUES (?, ?)", books_data)
+
+cursor.execute("DROP TABLE IF EXISTS rent_log")
+
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS rent_log (
+        id INTEGER PRIMARY KEY,
+        renter TEXT NOT NULL,
+        renter_id INTEGER NOT NULL,
+        title TEXT NOT NULL,
+        book_id INTEGER NOT NULL,
+        FOREIGN KEY(renter_id) REFERENCES members(id) ON DELETE CASCADE,
+        FOREIGN KEY(book_id) REFERENCES books(id) ON DELETE CASCADE
+    )
+""")
+
 
 con.commit()
 con.close()
