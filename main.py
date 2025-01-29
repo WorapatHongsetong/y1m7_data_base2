563072fd4930c30bc53ab4897172c3f021ad7273
QuantumTacoNinja <worapat1h@gmail.com>
2025-01-30 05:56:31 +0700

    Finished setup

---
+++ main.py
@@ -0,0 +1,9 @@
+import sqlite3
+import os
+
+db_folder = "db"
+if not os.path.exists(db_folder):
+    os.makedirs(db_folder)
+
+con = sqlite3.connect(f"{db_folder}/members.db")
+cursor = con.cursor()
